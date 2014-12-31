15881d2c7d2ad3a16fb91f662040fa62d7ec6143
Dan Schultz <schultz.t.dan@gmail.com>
2014-12-31 02:44:49 -0800

    Rename old tests to reactable tests

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -3,11 +3,13 @@ library frappe.hop_runner;
 import 'package:hop/hop.dart';
 import 'package:hop_unittest/hop_unittest.dart';
 import 'package:guinness/guinness.dart';
-import '../test/test_runner.dart' as TestRunner;
+import '../test/test_runner_reactables.dart' as test_runner_reactables;
+import '../test/test_runner_transformers.dart' as test_runner_transformers;
 
 void main(List args) {
   addTask("test", createUnitTestTask(() {
-    TestRunner.main();
+    test_runner_reactables.main();
+    test_runner_transformers.main();
 
     // Hop doesn't work with Guinness's auto-run behavior. We need to initialize
     // it manually. Hop will run any specs that are found since Guinness is using
