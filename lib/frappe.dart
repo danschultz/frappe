3d41304f95c22372e8e742caa5e6b3b33f67bcd1
Dan Schultz <schultz.t.dan@gmail.com>
2014-12-31 00:48:07 -0800

    Remove reactable classes that are now implemented as transforms

--- lib/frappe.dart
+++ lib/frappe.dart
@@ -6,17 +6,8 @@ import 'package:frappe/src/transformers.dart';
 
 part 'src/reactable.dart';
 part 'src/event_stream.dart';
-part 'src/forwarding_reactable.dart';
 part 'src/reactable_stream.dart';
-part 'src/merged_stream.dart';
-part 'src/take_until_reactable.dart';
-part 'src/skip_until_reactable.dart';
-part 'src/debounce_reactable.dart';
 part 'src/scan_reactable.dart';
-part 'src/delay_reactable.dart';
-part 'src/flat_map_latest_reactable.dart';
-part 'src/buffer_when_reactable.dart';
-part 'src/when_reactable.dart';
 
 part 'src/property.dart';
 part 'src/controller_property.dart';
--- lib/src/take_until_reactable.dart
+++
@@ -1,7 +0,0 @@
-part of frappe;
-
-class _TakeUntilReactable<T> extends _ForwardingReactable<T> {
-  _TakeUntilReactable(Reactable<T> reactable, Future stop) : super(reactable) {
-    stop.then((_) => close());
-  }
-}
