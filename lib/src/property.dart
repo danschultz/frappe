248cd6cc79802f4dfefb79455a3bf6507240fcdc
Dan Schultz <schultz.t.dan@gmail.com>
2015-02-26 13:50:11 -0800

    Fix a dart2js bug when initializing a Property

--- lib/src/property.dart
+++ lib/src/property.dart
@@ -20,9 +20,34 @@ class Property<T> extends Reactable<T> {
   bool get isBroadcast => _controller.stream.isBroadcast;
 
   Property._(Stream<T> stream, bool hasInitialValue, [T initialValue]) {
-    var input = stream.asBroadcastStream(onCancel: (subscription) => subscription.cancel());
     _hasCurrentValue = hasInitialValue;
     _currentValue = initialValue;
+    _controller = _createControllerForStream(stream);
+  }
+
+  /// Returns a new property where its current value is always [value].
+  factory Property.constant(T value) => new Property.fromStream(new Stream.fromIterable([value]));
+
+  /// Returns a new property where its current value is the latest value emitted
+  /// from [stream].
+  factory Property.fromStream(Stream<T> stream) => new Property._(stream, false);
+
+  /// Returns a new property where its starting value is [initialValue], and its
+  /// value after that is the latest value emitted from [stream].
+  factory Property.fromStreamWithInitialValue(T initialValue, Stream<T> stream) =>
+      new Property._(stream, true, initialValue);
+
+  /// Returns a new property where its current value is the completed value of
+  /// the [future].
+  factory Property.fromFuture(Future<T> future) => new Property.fromStream(new Stream.fromFuture(future));
+
+  /// Returns a new property where the starting value is [initialValue], and its
+  /// value after that is the value from [future].
+  factory Property.fromFutureWithInitialValue(T initialValue, Future<T> future) =>
+      new Property.fromStreamWithInitialValue(initialValue, new Stream.fromFuture(future));
+
+  StreamController _createControllerForStream(Stream stream) {
+    var input = stream.asBroadcastStream(onCancel: (subscription) => subscription.cancel());
 
     StreamSubscription subscription;
 
@@ -46,29 +71,8 @@ class Property<T> extends Reactable<T> {
       subscription = null;
     }
 
-    _controller = new StreamController.broadcast(onListen: onListen, onCancel: onCancel, sync: true);
+    return new StreamController.broadcast(onListen: onListen, onCancel: onCancel, sync: true);
   }
-
-  /// Returns a new property where its current value is always [value].
-  factory Property.constant(T value) => new Property.fromStream(new Stream.fromIterable([value]));
-
-  /// Returns a new property where its current value is the latest value emitted
-  /// from [stream].
-  factory Property.fromStream(Stream<T> stream) => new Property._(stream, false);
-
-  /// Returns a new property where its starting value is [initialValue], and its
-  /// value after that is the latest value emitted from [stream].
-  factory Property.fromStreamWithInitialValue(T initialValue, Stream<T> stream) =>
-      new Property._(stream, true, initialValue);
-
-  /// Returns a new property where its current value is the completed value of
-  /// the [future].
-  factory Property.fromFuture(Future<T> future) => new Property.fromStream(new Stream.fromFuture(future));
-
-  /// Returns a new property where the starting value is [initialValue], and its
-  /// value after that is the value from [future].
-  factory Property.fromFutureWithInitialValue(T initialValue, Future<T> future) =>
-      new Property.fromStreamWithInitialValue(initialValue, new Stream.fromFuture(future));
 
   StreamSubscription<T> listen(void onData(T value), {Function onError, void onDone(), bool cancelOnError}) {
     var controller = new StreamController(sync: true);
