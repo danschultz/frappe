a924d6c7eac7dd3d35dccbbfe7dbc596bcbd7fc8
Devon Carew <devoncarew@gmail.com>
2015-09-19 17:50:06 -0700

    add library dartdocs to the frappe library

--- lib/frappe.dart
+++ lib/frappe.dart
@@ -1,6 +1,10 @@
+/// A functional reactive programming library for Dart. Frappé extends the
+/// functionality of Dart's streams, and introduces new concepts like properties
+/// / signals.
 library frappe;
 
 import 'dart:async';
+
 import 'package:stream_transformers/stream_transformers.dart';
 
 part 'src/reactable.dart';
